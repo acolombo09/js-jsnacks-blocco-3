ae1ba8b072c7634b148118b80923c4b183dede98
acolombo09 <acolombo0911@gmail.com>
2023-07-14 15:49:19 +0200

    completed

--- jsnack3/js/script.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-/*
-Snack 3 (bonus)
-Fai inserire un numero, che chiameremo N, all’utente. 
-Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. 
-Ogni volta che ne crei uno, stampalo.
-*/
